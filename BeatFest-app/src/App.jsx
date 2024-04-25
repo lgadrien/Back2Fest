42080c2dd9708fd43b1d12760b4d19804e64d70a
Adrien <146069613+lgadrien@users.noreply.github.com>
2024-04-25 15:26:32 +0200

    Base du projet React+Vite et tailwind
    Base du site

---
+++ BeatFest-app/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+function App() {
+
+  return (
+    <> </>
+  )
+}
+
+export default App
