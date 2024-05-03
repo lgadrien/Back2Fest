f314dc937b2ddd2975a631148773273c2ea928be
marwan9105 <marwan.hassen@epitech.digital>
2024-05-03 11:04:33 +0200

    maj
    panier stocker dans les cookies

--- BeatFest-app/src/Components/Panier.jsx
+++ BeatFest-app/src/Components/Panier.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { usePanier } from './PanierContext';
+<<<<<<< Updated upstream
 
 function Panier() {
   const { panier } = usePanier();
@@ -13,8 +14,57 @@ function Panier() {
           <p>Prix unitaire: {article.prix}€</p>
         </div>
       ))}
+=======
+import { FaRegTrashAlt } from "react-icons/fa";
+import { CiCirclePlus, CiCircleMinus } from "react-icons/ci";
+
+function Panier() {
+  const { panier, viderPanier, modifierQuantite } = usePanier(); // Importez la fonction modifierQuantite depuis le contexte du panier
+
+  const prixTotal = panier.reduce((total, article) => {
+    return total + article.prix * article.quantite;
+  }, 0);
+
+  const handleClickViderPanier = () => {
+    viderPanier(); 
+  };
+
+  const handleAugmenterQuantite = (article) => {
+    modifierQuantite(article, article.quantite + 1);
+  };
+
+  const handleDiminuerQuantite = (article) => {
+    if (article.quantite > 1) {
+      modifierQuantite(article, article.quantite - 1);
+    }
+  };
+
+  return (
+    <div className='h-screen bg-slate-800'>
+      <div className='text-white text-2xl font-bold'>
+        <h2>Votre Panier</h2>
+        {panier.map((article, index) => (
+          <div key={index}>
+            <p>{article.name} - Quantité: {article.quantite}</p>
+            <p className='flex'>
+              <CiCirclePlus onClick={() => handleAugmenterQuantite(article)} />
+              <CiCircleMinus onClick={() => handleDiminuerQuantite(article)} />
+            </p>
+            <p>Prix unitaire: {article.prix}€</p>
+          </div>
+        ))}
+        <p>Prix total: {prixTotal.toFixed(2)}€</p>
+      </div>
+      <div className='text-white text-2xl'>
+        <FaRegTrashAlt onClick={handleClickViderPanier} /> {/* Ajoutez un gestionnaire d'événements pour vider le panier */}
+      </div>
+>>>>>>> Stashed changes
     </div>
   );
 }
 
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 export default Panier;
--- BeatFest-app/src/Components/PanierContext.jsx
+++ BeatFest-app/src/Components/PanierContext.jsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React, { createContext, useContext, useState } from 'react';
 
 const PanierContext = createContext();
@@ -8,13 +9,46 @@ export const usePanier = () => {
 
 export const PanierProvider = ({ children }) => {
   const [panier, setPanier] = useState([]);
+=======
+import React, { createContext, useContext, useState, useEffect } from 'react';
+import Cookies from 'js-cookie';
+
+const PanierContext = createContext();
+
+export const usePanier = () => useContext(PanierContext);
+
+export const PanierProvider = ({ children }) => {
+  const [panier, setPanier] = useState(() => {
+    const panierData = Cookies.get('panier');
+    return panierData ? JSON.parse(panierData) : [];
+  });
+
+  useEffect(() => {
+    if (!Cookies.get('panier')) {
+      Cookies.set('panier', JSON.stringify(panier));
+    }
+  }, []); // Définit le cookie une seule fois lors de l'initialisation du panier
+
+  useEffect(() => {
+    Cookies.set('panier', JSON.stringify(panier));
+  }, [panier]); // Met à jour le cookie uniquement lorsque le panier change
+>>>>>>> Stashed changes
 
   const ajouterAuPanier = (article) => {
     setPanier([...panier, article]);
   };
 
+<<<<<<< Updated upstream
   return (
     <PanierContext.Provider value={{ panier, ajouterAuPanier }}>
+=======
+  const viderPanier = () => {
+    setPanier([]);
+  };
+
+  return (
+    <PanierContext.Provider value={{ panier, ajouterAuPanier, viderPanier }}>
+>>>>>>> Stashed changes
       {children}
     </PanierContext.Provider>
   );
